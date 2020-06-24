2a7ccca062b55dc7bc1c3f94e437bda0681657d2
Alvaro Caxone <alvaro.caxone@mazzatech.com.br>
2020-06-23 22:47:40 -0300

    feature jump

--- scripts/sprite.js
+++ scripts/sprite.js
@@ -1,16 +1,37 @@
 class Sprite {
-    buildMatrix(elX = 4, elY = 4, width = 220, height = 270) {
-    let arr = [];
-    let positionX = 0;
-    let positionY = 0;
-    for (let i = 0; i < elX; i++) {
-      for (let j = 0; j < elY; j++) {
-        arr.push([positionX, positionY]);
-        positionX += width;
-      }
-      positionX = 0;
-      positionY += height;
+  constructor(
+    image,
+    matrix,
+    imageWidth, 
+    imageHeight, 
+    characterWidth, 
+    characterHeight,
+    positionX,
+  ) {
+    this.image = image;
+    this.matrix = matrix;
+    this.imageWidth = imageWidth;
+    this.imageHeight = imageHeight;
+    this.characterWidth = characterWidth;
+    this.characterHeight = characterHeight;
+    this.positionX = positionX;
+    this.positionY = height - characterHeight;
+    this.frame = 0;
+  }
+
+  show() {
+    image(
+      this.image, this.positionX, this.positionY, this.characterWidth, this.characterHeight,
+      this.matrix[this.frame][0], this.matrix[this.frame][1],
+      this.imageWidth, this.imageHeight
+    );
+    this.animation();
+  }
+
+  animation  () {
+    this.frame++;
+    if (this.frame >= this.matrix.length - 1) {
+      this.frame = 0;
     }
-    return arr;
   }
 }
--- sketch.js
+++ sketch.js
@@ -2,20 +2,37 @@ let scenarioImage;
 let characterImage;
 let scenario;
 let character;
+let slime;
 let soundtrack;
+let jumpSound;
 
 const ScenarioSpeed = 3;
 
 function preload() {
   characterImage = loadImage('assets/images/character/running.png');
   scenarioImage = loadImage('assets/images/scenario/forest.png');
+  enemyImage = loadImage('assets/images/enemies/slime.png');
   soundtrack = loadSound('assets/sounds/soundtrack.mp3');
+  jumpSound = loadSound('assets/sounds/jumpSound.mp3');
+}
+
+function keyPressed() {
+  if (key === 'ArrowUp') {
+    character.jump();
+    jumpSound.play()
+  }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   scenario = new Scenario(scenarioImage, ScenarioSpeed);
-  character = new Character(characterImage);
+
+  const characterMatrix = buildMatrix(4, 4, 220, 270);
+  character = new Character(characterImage, characterMatrix, 220, 270, 110, 135, 0);
+  
+  const slimeMatrix = buildMatrix(7, 4, 104, 104);
+  slime = new Enemy(enemyImage, slimeMatrix, 104, 104, 52, 52, width - 52);
+
   frameRate(30);
   soundtrack.loop();
 }
@@ -24,4 +41,11 @@ function draw() {
   scenario.show();
   scenario.move();
   character.show();
+  character.appliesGravity();
+  slime.show();
+  slime.move();
+  if(character.isColliding(slime)) {
+    console.log('colidiu');
+    noLoop();
+  }
 }
